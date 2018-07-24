b0a436e0d6e0b9cd3e09cb35d0d253cbf401294d
Derouiche Imed Eddine <derouicheimededdine@gmail.com>
2018-07-24 16:19:36 +0100

    relative path

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../album';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-tracklisting',
@@ -14,7 +14,7 @@ export class ProductTracklistingComponent implements OnInit {
   constructor(private _productService : ProductService) { }
 
   ngOnInit() {
-    
+
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
